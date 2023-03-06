8cad8d26b77ace62dfa601f314787d4ce1ef2ec3
Davide Marchi <davide.marchi@comuni-chiamo.com>
2023-03-06 12:38:57 +0100

    fixed broken method on werkzeug update

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='fdt-sqlalchemy',
-    version='0.0.4',
+    version='0.0.5',
     description='Flask-debugtoolbar configurable SQLAlchemy panel',
-    author='Charles Lavery',
+    author='Charles Lavery & Davide Marchi',
     author_email='charles.lavery@gmail.com',
     url='https://github.com/clavery/fdt-sqlalchemy',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'SQLAlchemy>=0.9.0',
-        'Flask-DebugToolbar>=0.9.0',
+        'SQLAlchemy>=2.0.4',
+        'Flask-DebugToolbar>=0.13.1',
     ],
     license='BSD',
     zip_safe=False,
